f2b129fefabd9c6db3899eef142afa1d01db187d
Sebastian Paweloszek <spaweloszek63@gmail.com>
2020-06-09 17:33:36 +0200

    setup

--- testsReactor/src/test/java/edu/iis/mto/testreactor/dishwasher/DishWasherTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/dishwasher/DishWasherTest.java
@@ -2,14 +2,33 @@ package edu.iis.mto.testreactor.dishwasher;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import edu.iis.mto.testreactor.dishwasher.engine.Engine;
+import edu.iis.mto.testreactor.dishwasher.pump.WaterPump;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 public class DishWasherTest {
 
-    @Test
-    public void itCompiles() {
-        assertThat(true, Matchers.equalTo(true));
+    DishWasher dishWasher;
+    ProgramConfiguration programConfiguration;
+
+    @Mock
+    WaterPump waterPump;
+    @Mock
+    Engine engine;
+    @Mock
+    DirtFilter dirtFilter;
+    @Mock
+    Door door;
+
+    @BeforeEach
+    public void setup() {
+        dishWasher = new DishWasher(waterPump, engine, dirtFilter, door);
     }
 
 }
